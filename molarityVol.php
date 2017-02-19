4c3775b39e3bf478633c31d33a2a47dcb984b2d7
medmin <medmin@163.com>
2017-02-19 17:01:06 +0800

    define the UI of molarityVolCalculator

--- molarityVol.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2017/2/17
- * Time: 17:39
- */
-
---
+++ molarityVol2.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>液体稀释计算器</title>
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.0/css/bootstrap-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.0/css/bootstrap.css"/>
+    <!--    <script type="text/javascript" src="css/bootstrap-3.3.0/js/bootstrap.js"></script>-->
+</head>
+<body>
+    <h2>液体稀释计算器</h2>
+    <form action="">
+        <p>母液摩尔浓度：
+            <input type="text">
+            <br>
+            <input type="radio">mol/L (M)
+            <input type="radio">mmol/L (mM)
+            <input type="radio">μmol/L (μM)
+        </p>
+        <p>母液体积：
+            <input type="text">
+            <br>
+            <input type="radio">L
+            <input type="radio">ml
+            <input type="radio">μl
+        </p>
+        <p>工作液摩尔浓度：
+            <input type="text">
+            <br>
+            <input type="radio">mol/L (M)
+            <input type="radio">mmol/L (mM)
+            <input type="radio">μmol/L (μM)
+        </p>
+        <p>工作液体积：
+            <input type="text">
+            <br>
+            <input type="radio">L
+            <input type="radio">ml
+            <input type="radio">μl
+        </p>
+    </form>
+    <button>计算</button>
+
+
+<!--jQuery-->
+<script type="text/javascript" src="js/jquery-3.1.1.js"></script>
+<script>
+
+</script>
+</body>
+</html>
