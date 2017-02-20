afbcc89d0e5cddc5e8b9f58b31b8daa3d5dc7926
medmin <medmin@163.com>
2017-02-20 15:04:05 +0800

    make sure the calculator fits the reality

--- molaritySoluteWeight.php
+++ molaritySoluteWeight.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>摩尔浓度计算器</title>
+		<title>配置:摩尔浓度</title>
         <!--Bootstrap CSS-->
         <!--<link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.0/css/bootstrap-theme.css"/>-->
         <!--<link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.0/css/bootstrap.css"/>-->
@@ -11,17 +11,17 @@
 
 	</head>
 	<body>
-        <h2>摩尔浓度计算器：</h2>
-		<form id="soluteWeightForm" action="molarityWeightCalculator.php" method="post">
-			<input type="text" id="solute"  placeholder="请输入溶质的分子量">
+        <h2>配置:摩尔浓度</h2>
+		<form id="soluteWeightForm" action="molaritySoluteWeightCalculator.php" method="post">
+			<input type="text" id="soluteMW"  placeholder="请输入溶质的分子量">
 			<br>
-			<input type="text" id="solutionVol" placeholder="请输入您要配制的体积">单位：ml
+            <input type="text" id="purity" placeholder="请输入溶质的纯度">%
+            <br>
+			<input type="text" id="solutionVol" placeholder="请输入您要配制的体积">ml
 			<br>
-			<input type="text" id="molarity" placeholder="请输入最终摩尔浓度">单位：mol/L (M)
-			<br>
-
+			<input type="text" id="solutionMolarity" placeholder="请输入最终摩尔浓度">mol/L (M)
 		</form>
-
+        <br>
         <button type="button" id="soluteWeightCalculatorBtn">计算</button>
         <button type="button" id="reloadBtn">刷新</button>
 		<div id="result1"><p></p></div>
@@ -44,12 +44,20 @@
 
 
 			$("#soluteWeightCalculatorBtn").click(function(){
-				var solute = $("#solute").val();
+				var soluteMW = $("#soluteMW").val();
+				var purity = $("#purity").val();
 				var solutionVol = $("#solutionVol").val();
-				var molarity = $("#molarity").val();
+				var solutionMolarity = $("#solutionMolarity").val();
 
-				if (solute != '' && solutionVol != '' && molarity != ''){
-                    $.post("molarityWeightCalculator.php", {solute:solute,solutionVol:solutionVol,molarity:molarity}, function(res){
+				if (soluteMW != '' && purity != '' && solutionVol != '' && solutionMolarity != ''){
+                    $.post("molaritySoluteWeightCalculator.php",
+                        {
+                            soluteMW:soluteMW,
+                            purity:purity,
+                            solutionVol:solutionVol,
+                            solutionMolarity:solutionMolarity
+                        },
+                        function(res){
                         $("#result1 p").text("您计算的结果为：" + res);
                     });
                 }
---
+++ molaritySoluteWeightCalculator.php
@@ -0,0 +1,37 @@
+<?php
+
+class Calculator {
+	
+	const GRAM = "g";
+	const stdSolutionVol = 1000;
+	const percentage =  0.01 ;
+	
+	public function getSoluteWeight($soluteMW, $purity, $solutionVol, $solutionMolarity) {
+		$soluteWeight = ($solutionVol / self::stdSolutionVol) *$solutionMolarity * $soluteMW / ($purity * self::percentage);
+		return number_format($soluteWeight, 2).self::GRAM;
+		// number_format's return value is string.
+	}
+	
+}
+
+$calculator = new Calculator();
+
+if (isset($_POST['soluteMW']) && isset($_POST['purity']) && isset($_POST['solutionVol']) && isset($_POST['solutionMolarity'])){
+    $soluteMW = $_POST['soluteMW'];
+	$purity = $_POST['purity'];
+	$solutionVol = $_POST['solutionVol'];
+    $solutionMolarity = $_POST['solutionMolarity'];
+}
+else {
+	$solute = 0;
+	$purity = 100 ;
+	$solutionVol = 0;
+	$molarity = 0;
+}
+
+
+$soluteWeight = $calculator->getSoluteWeight($soluteMW, $purity, $solutionVol, $solutionMolarity);
+
+
+//这里必须用echo，否则不能输出结果。
+echo $soluteWeight;
