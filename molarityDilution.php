6f4e873377c24adadaf6099e3a5cf5f3233f0675
medmin <medmin@163.com>
2017-02-21 15:39:37 +0800

    molarityDilution Calculator

--- molarityDilution.php
+++ molarityDilution.php
@@ -2,24 +2,30 @@
 <head>
     <meta charset="utf-8">
     <title>稀释计算器I：摩尔浓度</title>
-    <!--Bootstrap CSS-->
-    <!--<link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.0/css/bootstrap-theme.css"/>-->
-    <!--<link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.0/css/bootstrap.css"/>-->
-
-    <!--LayUI-->
-    <link rel="stylesheet" type="text/css" href="js/layui-v1.0.7/css/layui.css" />
-
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <link rel="stylesheet" href="css/weui.min.css" />
 </head>
 <body>
     <h2>稀释计算器I：摩尔浓度</h2>
     <form action="molarityDilutionCalculator.php" method="post" id="molarityVolForm">
-        <p>母液摩尔浓度：
-            <input type="text">
-            <br>
-            <input type="radio">mol/L (M)
-            <input type="radio">mmol/L (mM)
-            <input type="radio">μmol/L (μM)
-        </p>
+        <div>
+            <div class="weui-cell">
+                <div class="weui-cell__hd">
+                    <label class="weui-label">母液摩尔浓度</label>
+                </div>
+                <div class="weui-cell__bd">
+                    <input type="text" id="motherSolutionMolarityC" class="weui-input " placeholder="母液摩尔浓度">
+                </div>
+                <div class="weui-cell__ft">
+                    <select class="weui-select">
+                        <option value="">选择单位</option>
+                        <option value="">mol/L (M)</option>
+                        <option value="">mmol/L (mM)</option>
+                        <option value="">μmol/L (μM)</option>
+                    </select>
+                </div>
+            </div>
+        </div>
         <p>母液体积：
             <input type="text">
             <br>
@@ -47,10 +53,6 @@
 
     <!--jQuery-->
     <script type="text/javascript" src="js/jquery-3.1.1.js"></script>
-    <!--LayUI JS-->
-    <script type="text/javascript" src="js/layui-v1.0.7/layui.js"></script>
-    <!--Bootstrap JS-->
-    <!--<script type="text/javascript" src="css/bootstrap-3.3.0/js/bootstrap.js"></script>-->
 
     <script>
         $('#ReloadBtn').click(function () {
