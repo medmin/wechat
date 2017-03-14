8a9de77fc1868dbcb5e14d429c09dcad7b4045c3
medmin <medmin@163.com>
2017-03-14 18:01:32 +0800

    try to use curl to retrieve primers 1

--- qPCRPrimerDesign.php
+++ qPCRPrimerDesign.php
@@ -2,19 +2,46 @@
 <head>
     <meta charset="utf-8">
     <title>qPCR Primer Design v1.0</title>
+    <style>
+        .colorRed {
+            color : red;
+        }
+        .colorGreen {
+            color : green;
+        }
+    </style>
 </head>
 <body>
     <form action="qPCRPrimerDesignAPI.php" method="post">
-        <textarea name="" id="" cols="30" rows="10" placeholder="请输入基因序列"></textarea>
+        <textarea name="geneSeq" id="geneSeq" cols="30" rows="10" placeholder="请输入基因序列"></textarea>
         <br>
-        <input type="text" placeholder="请输入基因序列号" style="width: 231px" >
-        <button type="submit">开始设计引物</button>
-        <button type="reset">重新设计引物</button>
+        <input type="text" name="geneID" id="geneID" placeholder="请输入基因序列号" style="width: 231px" >
     </form>
+        <button type="button" id="PrimerDesignBtn">开始设计引物</button>
+        <br>
+        <button type="button" id="PrimerReloadBtn">重新设计引物</button>
+    <div id="PrimerDetails"></div>
 
 
+    <script type="text/javascript" src="js/jquery-3.1.1.js"></script>
     <script>
+        $("#PrimerDesignBtn").click(function () {
+            var geneSeq = $("#geneSeq").val();
 
+            if (geneSeq !== ""){
+                $.post("qPCRPrimerDesignAPI.php",
+                    {
+                        geneSeq : geneSeq,
+                    },
+                    function (result) {
+                        $("#PrimerDetails").text(result);
+                    }
+                );
+            }
+            else {
+                $("#PrimerDetails").text("请输入基因序列或基因标号！").addClass('colorRed');
+            }
+        });
     </script>
 </body>
 </html>
--- qPCRPrimerDesignAPI.php
+++ qPCRPrimerDesignAPI.php
@@ -1,6 +1,27 @@
 <?php
 
 $baseURL = "http://sg.idtdna.com";
+$setGeneSeqURL = "/primerquest/home/SetCSVValues";
+$checkFastaURL = "/primerquest/home/CheckFastaCount";
+$primerQuestRunURL = "/primerquest/home/primerquestrun";
+$resultURL = "/PrimerQuest/Home/Results";
+$primerDetailsURL = "/PrimerQuest/Home/Details/0_";
 
+$geneSeq = isset($_POST['geneSeq']) ? $_POST['geneSeq'] : false;
+$geneID = isset($_POST['geneID']) ? $_POST['geneID'] : false;
+
+//if (){
+//
+//}
 
 $ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $baseURL.$setGeneSeqURL);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $geneSeq);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+var_dump($result);
